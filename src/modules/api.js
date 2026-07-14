30b8271c5efa8b8af337ca4d0eb70a025bc3ec8e
Ian <dev@eonloop.com>
2026-07-14 14:34:52 -0600

    Updating api key as well as removing icon call from api.js

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,12 +1,8 @@
-const apiKey = 'Q3GQSXFKE8RPKCRZMPMVNSCP9';
+const apiKey = 'FC5QK7LP5DJS36JDRRNLUTG9U';
 const apiUrl = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
 
 export default async function getWeather(location) {
     const response = await fetch(`${apiUrl}${location}?key=${apiKey}`);
     const data = await response.json();
     return data;
-}
-
-export function getWeatherIcon(icon) {
-    return `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/icons/${icon}.svg`;
 }
